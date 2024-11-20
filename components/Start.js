43c29c785dcac08c2f7484eab9ea29c1da4e32f7
sergiorsj11 <sergiorsj11@gmail.com>
2024-11-20 15:41:49 -0300

    msg

--- components/Start.js
+++ components/Start.js
@@ -4,13 +4,18 @@ import imageBackground  from "../images/BackgroundImage.png";
 const Start = ({ navigation }) => {
  return (
    <View style={styles.container}>
-    <ImageBackground style={styles.image} source={imageBackground} resizeMode="cover" style={styles.image}>
+    <ImageBackground style={styles.image} source={imageBackground} resizeMode="cover">
+    <Text style={styles.text}>Chat App!</Text>
+    <View style={styles.containerWhite}>
      <Text>Hello Screen1!</Text>
-     <Button
-       title="Go to Screen 2"
-       onPress={() => navigation.navigate('Screen2')}
+     <Button style={styles.buttonStartChatting}
+       title="Start Chatting"
+       color="#ffffff"
+       onPress={() => navigation.navigate('Chat')}
      />
+     </View>
      </ImageBackground>
+
    </View>
  );
 }
@@ -21,12 +26,35 @@ const styles = StyleSheet.create({
    justifyContent: 'center',
    alignItems: 'center'
  },
+ containerWhite: {
+    flex: 1,
+    width: "88%",
+    width: "44%",
+    backgroundColor: "white",
+    bottom: 0,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
  image: {
     flex: 1,
     width: "100%",
     height: "100%",
     justifyContent: 'center',
    alignItems: 'center'
+ },
+ buttonStartChatting: {
+    backgroundColor: "#757083",
+    borderRadius: 8,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    marginTop: 10
+ },
+ text: {
+    padding: "25%",
+    flex: 6,
+    fontSize: 45,
+    color: "white",
+    fontWeight: "600"
  }
 });
 
