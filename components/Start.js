d96b5599205d784461789f112b60b3b088d25fb3
sergiorsj11 <sergiorsj11@gmail.com>
2024-11-20 15:58:44 -0300

    msg

--- components/Start.js
+++ components/Start.js
@@ -1,13 +1,27 @@
-import { StyleSheet, View, Text, Button, ImageBackground } from 'react-native';
+import { StyleSheet, View, Text, Button, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
 import imageBackground  from "../images/BackgroundImage.png";
+import { useState } from 'react';
 
 const Start = ({ navigation }) => {
+    const [name, setname] = useState("")
  return (
    <View style={styles.container}>
     <ImageBackground style={styles.image} source={imageBackground} resizeMode="cover">
     <Text style={styles.text}>Chat App!</Text>
     <View style={styles.containerWhite}>
-     <Text>Hello Screen1!</Text>
+        <View style={styles.inputContainer}> 
+        <TextInput
+        style={styles.input}
+       value={name}
+       onChangeText={setname}
+       placeholder='Your Name'
+     /></View>
+   <Text style={styles.text1}>Choose Background Color:</Text>
+   <View>
+   <TouchableOpacity style={styles.colorbutton}>
+          <Text>Press Here</Text>
+        </TouchableOpacity>
+    </View>
      <Button style={styles.buttonStartChatting}
        title="Start Chatting"
        color="#ffffff"
@@ -55,6 +69,37 @@ const styles = StyleSheet.create({
     fontSize: 45,
     color: "white",
     fontWeight: "600"
+ },
+ text1: {
+    color: "#757083",
+    fontSize: 16,
+    fontWeight: "300",
+    marginTop: 20
+ },
+ inputContainer : {
+    flexDirection: "row",
+   alignItems: 'center',
+   borderColor: "#757083",
+   borderWidth: 2,
+   padding: 18, 
+   marginLeft: 20, 
+   marginRight: 20,
+   marginTop: -10,
+   marginBottom: 10
+ },
+ input: {
+    flex: 1,
+    marginLeft: 10,
+    fontSize: 16,
+    fontWeight: "300"
+ },
+ colorbutton: {
+    borderRadius: 20
+ },
+ colorbuttoncontainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 20
  }
 });
 
