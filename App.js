be754f9a6bd0bdde325e07030a1e6041a16d05d7
sergiorsj11 <sergiorsj11@gmail.com>
2024-12-11 11:20:34 -0500

    Revert "w"
    This reverts commit 7bd5016776d558b096bce0017e809a5929c7d40d.

--- App.js
+++ App.js
@@ -1,36 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, Text, View } from 'react-native';
-import Start from './components/Start';
-import Chat from './components/Chat';
-
-
-const Stack = createNativeStackNavigator();
-
+// import react Navigation
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+// Create the navigator
+const Stack = createNativeStackNavigator();
+
 import { initializeApp } from "firebase/app";
-import { getFirestore, disableNetwork, enableNetwork } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
-import { useNetInfo }from '@react-native-community/netinfo';
-import { useEffect } from 'react';
+// import the screens
+import ShoppingLists from './components/ShoppingLists';
 
-import { getStorage } from "firebase/storage";
-
-
-export default function App() {
-
-  const connectionStatus = useNetInfo();  
-
-  useEffect(() => {
-    if (connectionStatus.isConnected === false) {
-      Alert.alert("Connection Lost!");
-      disableNetwork(db);
-    } else if (connectionStatus.isConnected === true) {
-      enableNetwork(db);
-    }
-  }, [connectionStatus.isConnected]);
-
+const App = () => {
   const firebaseConfig = {
     apiKey: "AIzaSyBIvraaC5MRprYFymcoZjZpEGpNwakzwNg",
   authDomain: "chatapp-2436a.firebaseapp.com",
@@ -40,37 +21,27 @@ export default function App() {
   appId: "1:999843738259:web:5d46f0974556e070e41366",
   measurementId: "G-53JV80Z5Q7"
   };
-  
+
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
 
   // Initialize Cloud Firestore and get a reference to the service
   const db = getFirestore(app);
 
-  const storage = getStorage(app);
-
-
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Start">
+        initialRouteName="ShoppingLists"
+      >
         <Stack.Screen
-          name="Start"
-          component={Start}/>
-        <Stack.Screen
-          name="Chat">
-            {props => <Chat isConnected={connectionStatus.isConnected} db={db} storage={storage} {...props}/>}
+          name="ShoppingLists"
+        >
+          {props => <ShoppingLists db={db} {...props} />}
         </Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   );
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
